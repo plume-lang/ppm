d7042d5c2c3fbc6a9ea0ee32259acf585f9d0bf0
Sisypheus <contact@sisypheus.dev>
2024-06-13 18:14:58 +0200

    feat(ffi): added some spawn_process functions

--- library/ppm-ffi.js
+++ library/ppm-ffi.js
@@ -2,8 +2,17 @@ function createDatatype(datatypeName, variantName) {
   return (...values) => [null, datatypeName, variantName, ...values];
 }
 
+function callLambda(lambda) {
+  const env = lambda["0"];
+  const handler = lambda["1"];
+
+  return (...args) => handler(env, ...args);
+}
+
 const None = createDatatype('Option', 'None');
 const Some = createDatatype('Option', 'Some');
+
+const tuple = createDatatype('tuple', 'tuple');
 
 const child_process = require('child_process');
 const fs = require('fs');
@@ -46,13 +55,53 @@ module.exports = {
         stderr += data;
       });
 
-      child.on('close', (code) => {
-        if (code === 0) {
-          resolve([null, 'tuple', 'tuple', code, stdout.trim()]);
-        } else {
-          resolve([null, 'tuple', 'tuple', code, stdout.trim()]);
-        }
-      });
+      child.on('close', (code) => 
+        resolve(tuple(code, stdout.trim(), stderr.trim()))
+      );
     });
-  }
+  },
+
+  async spawn_process_callback(cmd, args, callback) {
+    return new Promise((resolve) => {
+      const child = child_process.spawn(cmd, args);
+      let stdout = '';
+      let stderr = '';
+
+      child.stdout.on('data', (data) => {
+        callLambda(callback)(data);
+        stdout += data;
+      });
+
+      child.stderr.on('data', (data) => {
+        stderr += data;
+      });
+
+      child.on('close', (code) => 
+        resolve(tuple(code, stdout.trim(), stderr.trim()))
+      );
+    });
+  },
+
+  async spawn_process_to_terminal(cmd, args) {
+    return new Promise((resolve) => {
+      const child = child_process.spawn(cmd, args);
+      let stdout = '';
+      let stderr = '';
+
+      child.stdout.pipe(process.stdout);
+      child.stderr.pipe(process.stderr);
+
+      child.stdout.on('data', (data) => {
+        stdout += data;
+      });
+
+      child.stderr.on('data', (data) => {
+        stderr += data;
+      });
+
+      child.on('close', (code) => 
+        resolve(tuple(code, stdout.trim(), stderr.trim()))
+      );
+    });
+  },
 }
